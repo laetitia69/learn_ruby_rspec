74c0d709df4c676bcda9935e953cf797ded467c3
laetitia69 <titi-fernandes@hotmail.fr>
2018-07-09 14:21:53 +0200

    niveau calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add(x, y)
+	return x + y
+end
+
+def subtract(x, y)
+	return x - y
+end
+
+def sum(array)
+	return array.sum
+end
+
+def multiply(x, y)
+	return x * y
+end
+
+def power(x, y)
+	return x.pow(y)
+end
+
+def factorial(x, y)
+	fraction = Rational(x, y)
+end
