d1719ab313638d098ae8af914a320c5ef815edbc
laetitia69 <titi-fernandes@hotmail.fr>
2018-07-09 16:24:26 +0200

    calcul

--- 03_basics/basics.rb
+++ 03_basics/basics.rb
@@ -1,1 +1,14 @@
 # write your code here
+def who_is_bigger(a, b, c)
+	if !nil
+			
+		if a > b && a > c
+			return a
+		end
+		elsif b > a && b > c
+			return b
+		end
+	else c > a && c > b
+		return c
+end
+
